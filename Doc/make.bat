85b44c30a7cc7a9eda2754060e8fcaff3e1f25c4
cocoatomo <cocoatomo77@gmail.com>
2017-02-28 15:27:54 +0900

    Avoid sed inconpatibility (#1)

--- Doc/make.bat
+++ Doc/make.bat
@@ -40,6 +40,9 @@ if "%1" == "clean" (
     goto end
 )
 
+rem JP: i18n target
+if "%1" EQU "gettext" goto gettext
+
 %SPHINXBUILD% >nul 2> nul
 if errorlevel 9009 (
     echo.
@@ -124,6 +127,12 @@ goto end
 
 :serve
 cmd /C %PYTHON% ..\Tools\scripts\serve.py %BUILDDIR%\html
+goto end
+
+:gettext
+cmd /C %SPHINXBUILD% -E -b gettext -D gettext_compact=0  -w warnings.txt -d build\.doctrees . locale\pot
+cmd /C %PYTHON% tools\delete_pot_creation_date.py locale\pot
+
 goto end
 
 :end
